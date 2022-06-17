8966dcaa2c49bf69e84eefd9025c3087113c1d6d
Alimardon9459 <mullajonovalimardon@gmail.com>
2022-06-17 22:17:06 +0500

    frist comment

---
+++ src/components/Map.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="h-100vh" >
+        <h1>sizning joylashgan o'rningiz</h1>
+        <p>{{coordinates.lat }}  uzunlik, {{coordinates.lng}} kenglik </p>
+        <GmapMap
+            :center="{lat:10, lng:10}"
+            :zoom="7"
+            map-type-id="terrain"
+            style="width:650px; height: 365px"
+            />
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            coordinates:{
+                lat:0 ,
+                lng:0
+            }
+        }
+    },
+    created(){
+            this.$getLocation(options)
+                .then(coordinates => {
+            console.log(coordinates);
+        })
+        
+    }
+}
+</script>
+<style scoped>
+    
+</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueGeolocation from 'vue-browser-geolocation';
+
 
 Vue.config.productionTip = false
-
+Vue.use(VueGeolocation);
+  
+import *as VueGoogeleMap from 'vue2-google-maps'
+Vue.use(VueGoogeleMap, {
+  load:{
+    key:''
+  },
+  isntallComponets:false
+} )
+ 
 new Vue({
   router,
   store,
